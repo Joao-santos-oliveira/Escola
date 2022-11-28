5aba8fadc2dc570a14a7ca26ffd565cd07f78132
Joao-santos-oliveira <80595573+Joao-santos-oliveira@users.noreply.github.com>
2022-11-27 21:03:38 -0300

    Finalizando

--- Script.js
+++ Script.js
@@ -14,6 +14,7 @@ function botao(){
     if(nome.value===""){
      alert ('Campo vazio... Por favor digite o nome do experimento');
     }
+    
     else if (nome.value==='Óptica'){
         img.innerHTML = '<img src="../Bia.jpeg" class="imagens">'
         oi.innerHTML = 'Materias necessários:'
@@ -25,6 +26,85 @@ function botao(){
         oi7.innerHTML = 'Feito por:'
         oi8.innerHTML = 'Beatriz'
     }
+    else if (nome.value==='Óptica '){
+        img.innerHTML = '<img src="../Bia.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Uma imagem e um copo com água'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Coloque o copo com água em frente da imagem escolhida.'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A experiência tem como objetivo demonstrar o fenômeno da refração. Com o desvio sofrido pela luz (refração) na mudança de um meio para outro'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML = 'Beatriz'
+    }
+    else if (nome.value==='óptica'){
+        img.innerHTML = '<img src="../Bia.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Uma imagem e um copo com água'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Coloque o copo com água em frente da imagem escolhida.'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A experiência tem como objetivo demonstrar o fenômeno da refração. Com o desvio sofrido pela luz (refração) na mudança de um meio para outro'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML = 'Beatriz'
+    }
+    else if (nome.value==='óptica '){
+        img.innerHTML = '<img src="../Bia.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Uma imagem e um copo com água'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Coloque o copo com água em frente da imagem escolhida.'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A experiência tem como objetivo demonstrar o fenômeno da refração. Com o desvio sofrido pela luz (refração) na mudança de um meio para outro'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML = 'Beatriz'
+    }
+    else if (nome.value==='Optica'){
+        img.innerHTML = '<img src="../Bia.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Uma imagem e um copo com água'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Coloque o copo com água em frente da imagem escolhida.'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A experiência tem como objetivo demonstrar o fenômeno da refração. Com o desvio sofrido pela luz (refração) na mudança de um meio para outro'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML = 'Beatriz'
+    }
+    else if (nome.value==='Optica '){
+        img.innerHTML = '<img src="../Bia.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Uma imagem e um copo com água'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Coloque o copo com água em frente da imagem escolhida.'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A experiência tem como objetivo demonstrar o fenômeno da refração. Com o desvio sofrido pela luz (refração) na mudança de um meio para outro'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML = 'Beatriz'
+    }
+    else if (nome.value==='optica'){
+        img.innerHTML = '<img src="../Bia.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Uma imagem e um copo com água'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Coloque o copo com água em frente da imagem escolhida.'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A experiência tem como objetivo demonstrar o fenômeno da refração. Com o desvio sofrido pela luz (refração) na mudança de um meio para outro'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML = 'Beatriz'
+    }
+    else if (nome.value==='optica '){
+        img.innerHTML = '<img src="../Bia.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Uma imagem e um copo com água'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Coloque o copo com água em frente da imagem escolhida.'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A experiência tem como objetivo demonstrar o fenômeno da refração. Com o desvio sofrido pela luz (refração) na mudança de um meio para outro'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML = 'Beatriz'
+    }
+
+
     else if (nome.value==='Cobra de espuma'){
         img.innerHTML = '<img src="../Maria clara.jpeg" class="imagens">'
         oi.innerHTML = 'Materias necessários:'
@@ -36,6 +116,41 @@ function botao(){
         oi7.innerHTML = 'Feito por:'
         oi8.innerHTML =  'Maria Clara'    
     } 
+    else if (nome.value==='Cobra de espuma '){
+        img.innerHTML = '<img src="../Maria clara.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Uma garrafa pet; Um pote com água; Detergente; Um pano; Objeto perfurante (faca, tesoura, etc..); um elástico'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Com o objeto perfurante, corte o fundo da garrafa, e em seguida a cubra com o pano. Após misturar a água com o detergente, mergulhe a garrafa e assopre para fazer a "Cobra de Espuma" '
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A formação da "Cobra de Espuma, se deve pela reação química ocorrida no contado entre o detergente e a água, essa junção fortalece as ligações de hidrogênio, possibilitando o fenômeno observado'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML =  'Maria Clara'    
+    } 
+    else if (nome.value==='cobra de espuma'){
+        img.innerHTML = '<img src="../Maria clara.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Uma garrafa pet; Um pote com água; Detergente; Um pano; Objeto perfurante (faca, tesoura, etc..); um elástico'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Com o objeto perfurante, corte o fundo da garrafa, e em seguida a cubra com o pano. Após misturar a água com o detergente, mergulhe a garrafa e assopre para fazer a "Cobra de Espuma" '
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A formação da "Cobra de Espuma, se deve pela reação química ocorrida no contado entre o detergente e a água, essa junção fortalece as ligações de hidrogênio, possibilitando o fenômeno observado'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML =  'Maria Clara'    
+    }  
+     else if (nome.value==='cobra de espuma '){
+        img.innerHTML = '<img src="../Maria clara.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Uma garrafa pet; Um pote com água; Detergente; Um pano; Objeto perfurante (faca, tesoura, etc..); um elástico'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Com o objeto perfurante, corte o fundo da garrafa, e em seguida a cubra com o pano. Após misturar a água com o detergente, mergulhe a garrafa e assopre para fazer a "Cobra de Espuma" '
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A formação da "Cobra de Espuma, se deve pela reação química ocorrida no contado entre o detergente e a água, essa junção fortalece as ligações de hidrogênio, possibilitando o fenômeno observado'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML =  'Maria Clara'    
+    } 
+
+
     else if (nome.value==='Densidade'){
         img.innerHTML = '<img src="../Priscila.jpeg" class="imagens">'
         oi.innerHTML = 'Materias necessários:'
@@ -47,6 +162,86 @@ function botao(){
         oi7.innerHTML = 'Feito por:'
         oi8.innerHTML =  'Priscila'   
     } 
+    else if (nome.value==='Densidade '){
+        img.innerHTML = '<img src="../Priscila.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Óleo vegetal; Corante; Comprimido efervescente; Recipiente transparente com tampa.'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Coloque 200ml de água no recipiente, e em seguida adicione 4 a 8 colheres de corante. Complete o recipiente com óleo vegetal, deixando um espaço de 3 dedos para ocorrer a reação. Agora basta adicionar o comprimido efervescente, e a sua "Lâmpada de Lava" estará pronta.'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A pastilha efervescente ao entrar em contato com a água, ela vai liberar gás carbônico, e como  o ar é mais leve que a água e óleo, ele vai subir. Nesse movimento o gás também transporta um pouco de água, agindo como uma espécie de boia.'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML =  'Priscila'   
+    } 
+    else if (nome.value==='densidade'){
+        img.innerHTML = '<img src="../Priscila.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Óleo vegetal; Corante; Comprimido efervescente; Recipiente transparente com tampa.'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Coloque 200ml de água no recipiente, e em seguida adicione 4 a 8 colheres de corante. Complete o recipiente com óleo vegetal, deixando um espaço de 3 dedos para ocorrer a reação. Agora basta adicionar o comprimido efervescente, e a sua "Lâmpada de Lava" estará pronta.'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A pastilha efervescente ao entrar em contato com a água, ela vai liberar gás carbônico, e como  o ar é mais leve que a água e óleo, ele vai subir. Nesse movimento o gás também transporta um pouco de água, agindo como uma espécie de boia.'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML =  'Priscila'   
+    } 
+    else if (nome.value==='densidade '){
+        img.innerHTML = '<img src="../Priscila.jpeg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Óleo vegetal; Corante; Comprimido efervescente; Recipiente transparente com tampa.'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Coloque 200ml de água no recipiente, e em seguida adicione 4 a 8 colheres de corante. Complete o recipiente com óleo vegetal, deixando um espaço de 3 dedos para ocorrer a reação. Agora basta adicionar o comprimido efervescente, e a sua "Lâmpada de Lava" estará pronta.'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'A pastilha efervescente ao entrar em contato com a água, ela vai liberar gás carbônico, e como  o ar é mais leve que a água e óleo, ele vai subir. Nesse movimento o gás também transporta um pouco de água, agindo como uma espécie de boia.'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML =  'Priscila'   
+    } 
+
+
+    else if (nome.value==='Luz'){
+        img.innerHTML = '<img src="../Luz.jpg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Laser; Garrafa pet e Canudinho'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Faça um furo na garrafa do mesmo diâmetro do canudinho, após isso cole o canudinho na garrafa e deixe ele todo por fora da garrafa. Feito isso, apague a luz e jogue o laser dentro do fluxo da água'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'Aluz segue a curvatura da trajetória da água fazendo um zigue zague refletindo completamente quando chega na divisa entre a água e o ar voltando para dentro da água batendo na outra parede da água refletindo completamente mais uma vez voltando para dentro e fazendo um caminho em zigue zague seguindo a trajetória curva da água'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML =  'Lucas, Inácio e Davi'   
+    }
+    else if (nome.value==='Luz '){
+        img.innerHTML = '<img src="../Luz.jpg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Laser; Garrafa pet e Canudinho'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Faça um furo na garrafa do mesmo diâmetro do canudinho, após isso cole o canudinho na garrafa e deixe ele todo por fora da garrafa. Feito isso, apague a luz e jogue o laser dentro do fluxo da água'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'Aluz segue a curvatura da trajetória da água fazendo um zigue zague refletindo completamente quando chega na divisa entre a água e o ar voltando para dentro da água batendo na outra parede da água refletindo completamente mais uma vez voltando para dentro e fazendo um caminho em zigue zague seguindo a trajetória curva da água'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML =  'Lucas, Inácio e Davi'   
+    }
+    else if (nome.value==='luz'){
+        img.innerHTML = '<img src="../Luz.jpg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Laser; Garrafa pet e Canudinho'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Faça um furo na garrafa do mesmo diâmetro do canudinho, após isso cole o canudinho na garrafa e deixe ele todo por fora da garrafa. Feito isso, apague a luz e jogue o laser dentro do fluxo da água'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'Aluz segue a curvatura da trajetória da água fazendo um zigue zague refletindo completamente quando chega na divisa entre a água e o ar voltando para dentro da água batendo na outra parede da água refletindo completamente mais uma vez voltando para dentro e fazendo um caminho em zigue zague seguindo a trajetória curva da água'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML =  'Lucas, Inácio e Davi'   
+    }
+    else if (nome.value==='luz '){
+        img.innerHTML = '<img src="../Luz.jpg" class="imagens">'
+        oi.innerHTML = 'Materias necessários:'
+        oi2.innerHTML = 'Laser; Garrafa pet e Canudinho'
+        oi3.innerHTML = 'Como fazer:'
+        oi4.innerHTML = 'Faça um furo na garrafa do mesmo diâmetro do canudinho, após isso cole o canudinho na garrafa e deixe ele todo por fora da garrafa. Feito isso, apague a luz e jogue o laser dentro do fluxo da água'
+        oi5.innerHTML = 'Explicação Científica:'
+        oi10.innerHTML = 'Aluz segue a curvatura da trajetória da água fazendo um zigue zague refletindo completamente quando chega na divisa entre a água e o ar voltando para dentro da água batendo na outra parede da água refletindo completamente mais uma vez voltando para dentro e fazendo um caminho em zigue zague seguindo a trajetória curva da água'
+        oi7.innerHTML = 'Feito por:'
+        oi8.innerHTML =  'Lucas, Inácio e Davi'   
+    }
+
     else{
         oi.innerHTML = 'Esse projeto não existe, verifique se escreveu da maneira correta'
     }
